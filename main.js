5933009d7f2f5a887fa7c7b31bc0a4b67095c921
Tri7013 <Phmt16022003@gmail.com>
2025-03-04 16:06:47 +0700

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-document.getElementById("name").textContent = "Phan Huỳnh Minh Trí";
+document.getElementById("name").innerHTML = `
+    <strong>Phan Huỳnh Minh Trí</strong><br>
+    🏫 Lớp: 21DHTD3<br>
+    🎓 Sinh viên: Công Nghệ Thông Tin
+`;
